e87f6c510210ca841f64ad8ce47b5ea5480584f1
Arjun Nahar <ArjunNahar@dunsterhouse.co.uk>
2023-09-12 16:38:03 +0100

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { Modal } from "@/components/ui/modal";
+import { UserButton } from "@clerk/nextjs";
+
+const SetupPage = () => {
+    return (
+      <div className="p-4">
+        <Modal title="Test" description="Test Desc" isOpen={true} onClose={()=>{}}>
+          Children
+        </Modal>
+      </div>
+    )
+  }
+
+export default SetupPage;
+  
