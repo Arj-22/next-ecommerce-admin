08286d9a9a87c1fb2e5e4d325cb40f8233924413
Arjun Nahar <ArjunNahar@dunsterhouse.co.uk>
2023-09-20 10:21:23 +0100

    changed archive on checkout

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -57,7 +57,7 @@ export async function POST(req: Request) {
                 }
             },
             data:{
-                isArchived: true
+                isArchived: false
             }
         })
     }
